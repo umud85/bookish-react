8ec11f2c1711d0e9d3fa17a8f952bdc99bccbc82
Umut Akbayin <umutakbayin@posteo.de>
2023-05-25 13:06:22 +0200

    refactor with booklist component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
+import BookList from './BookList';
 
  function App() {
+     const books = [
+         { name: 'Refactoring' },
+         { name: 'Domain-driven design' },
+     ];
+
     return (
       <div className='App'>
         <Typography variant='h2' component='h2' data-test='heading'>
           Bookish
         </Typography>
+        <BookList books={books} />
       </div>
     );
  }
